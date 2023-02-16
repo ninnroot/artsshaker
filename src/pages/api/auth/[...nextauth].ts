1cec2f9a2813630c0b49ca026c9094b90f8370d1
ninnroot <misterjames.thiha@gmail.com>
2023-02-16 17:35:12 +0800

    testing out oauth again

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,19 @@
 import NextAuth from 'next-auth/next';
 import DiscordProvider from 'next-auth/providers/discord';
+import { signIn } from 'next-auth/react';
 
 export default NextAuth({
   providers: [
     DiscordProvider({
       clientId: String(process.env['DISCORD_CLIENT_ID']),
       clientSecret: String(process.env['DISCORD_CLIENT_SECRET']),
-      // authorization:"http://localhost:3000/api/auth/callback/discord/authorize"
+      authorization:"https://localhost:443/api/auth/callback/discord/authorize"
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET
+  callbacks: {
+    async signIn({user, account, profile}){
+        console.log(user, account, profile)
+        return true
+    }
+  }
 });
