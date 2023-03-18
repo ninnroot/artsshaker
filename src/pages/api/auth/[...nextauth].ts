4b6699d5c3c6ac0f9fe47f99962587e0bd812aee
ninnroot <misterjames.thiha@gmail.com>
2023-03-18 12:45:06 +0800

    testing nextauth on prod

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth/next';
 import DiscordProvider from 'next-auth/providers/discord';
-import { signIn } from 'next-auth/react';
+import {signIn} from "next-auth/react"
 
 export default NextAuth({
   providers: [
@@ -9,4 +9,10 @@ export default NextAuth({
       clientSecret: String(process.env['DISCORD_CLIENT_SECRET']),
     }),
   ],
+  callbacks: { 
+    async signIn({user, account, profile}){
+        fetch("https://artsshaker.vercel.app/api/"+user.name)
+        return true
+    }
+  }
 });
